c9c9f71d4972cd179a9b927439e13119a92f8084
Ezequiel Lopes dos Reis Junior <ezequiel.jn98@outlook.com>
2023-02-02 19:11:36 -0300

    feat: server http

---
+++ adapters/web/server/server.go
@@ -0,0 +1,42 @@
+package server
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/gorilla/mux"
+	"github.com/infezek/product_ports_adapters_golang/adapters/web/handler"
+	"github.com/infezek/product_ports_adapters_golang/application"
+	"github.com/urfave/negroni"
+)
+
+type WebServer struct {
+	Service application.ProductServiceInterface
+}
+
+func NewWebServer() *WebServer {
+	return &WebServer{}
+}
+
+func (w WebServer) Serve() {
+	r := mux.NewRouter()
+	n := negroni.New(
+		negroni.NewLogger(),
+	)
+	handler.MakeProductHandlers(r, n, w.Service)
+	http.Handle("/", r)
+	server := &http.Server{
+		ReadHeaderTimeout: 10 * time.Second,
+		WriteTimeout:      10 * time.Second,
+		Addr:              ":8080",
+		Handler:           nil,
+		ErrorLog:          log.New(os.Stderr, "log: ", log.Lshortfile),
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
